032a6c0871a5dafc33b43f54c3a070fb0c1b577c
Yossan Rahmadi <yossansr06@gmail.com>
2020-08-06 01:01:10 +0800

    Initial commit

---
+++ Clima/Model/WeatherData.swift
@@ -0,0 +1,24 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Yossan Rahmadi on 08/07/20.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData: Codable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Codable {
+    let temp: Double
+}
+
+struct Weather: Codable {
+    let description: String
+    let id: Int
+}
